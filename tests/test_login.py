a9b31c04bfc3b08cad4bbf14843db6585045a26b
Prem123e <premkumarkanuganti123@gmail.com>
2026-09-09 22:05:39 +0530

    Day 2: Implement successful login automation using Playwright locators

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,6 +1,6 @@
 from playwright.sync_api import Page, expect
 
-def test_open_saucedemo(page:Page):
+def test_login_with_valid_credentials(page:Page):
 
     page.goto("https://www.saucedemo.com/")
     expect(page).to_have_title("Swag Labs")
@@ -8,7 +8,6 @@ def test_open_saucedemo(page:Page):
     page.get_by_placeholder("Username").fill("standard_user")
     page.get_by_placeholder("Password").fill("secret_sauce")
     page.get_by_role("button", name="Login").click()
-    page.wait_for_timeout(5000)
     expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
     expect(page.locator(".title")).to_have_text("Products")
     print(page.locator(".inventory_item").count())
