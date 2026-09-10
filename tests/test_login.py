ae1f80263e29eeda92b6b49e76a267ecf14f2d54
Prem123e <premkumarkanuganti123@gmail.com>
2026-09-10 21:28:01 +0530

    Day 3: Implement Page Object Model for login page

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,15 +1,14 @@
-from playwright.sync_api import Page, expect
+from playwright.sync_api import expect
+from pages.login_page import LoginPage
 
-def test_login_with_valid_credentials(page:Page):
+def test_login_with_valid_credentials(page):
 
-    page.goto("https://www.saucedemo.com/")
-    expect(page).to_have_title("Swag Labs")
+    login_page=LoginPage(page)
+    login_page.navigate()
+    print(page.url)
     print(page.title())
-    page.get_by_placeholder("Username").fill("standard_user")
-    page.get_by_placeholder("Password").fill("secret_sauce")
-    page.get_by_role("button", name="Login").click()
+    login_page.login("standard_user","secret_sauce")
     expect(page).to_have_url("https://www.saucedemo.com/inventory.html")
     expect(page.locator(".title")).to_have_text("Products")
-    print(page.locator(".inventory_item").count())
 
 
