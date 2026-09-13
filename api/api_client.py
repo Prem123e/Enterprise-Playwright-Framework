8ccb044322107e7d06872da6d644aa32093e4083
Prem123e <premkumarkanuganti123@gmail.com>
2026-09-13 16:24:31 +0530

    "Day 13: Add PUT PATCH and DELETE API tests"

--- api/api_client.py
+++ api/api_client.py
@@ -7,3 +7,9 @@ class APIClient:
 
     def post(self,url:str,data:dict):
         return requests.post(url,json=data)
+    def put(self,url:str,data:dict):
+        return requests.put(url,json=data)
+    def patch(self,url:str,data:dict):
+        return requests.patch(url,json=data)
+    def delete(self,url:str):
+        return requests.delete(url)
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,6 @@
 from api.api_client import APIClient
-
-client = APIClient()
 def test_get_single_post():
+    client = APIClient()
     response = client.get(
         "https://jsonplaceholder.typicode.com/posts/1"
     )
@@ -14,16 +13,15 @@ def test_get_single_post():
     print(response_data)
 
 def test_create_post():
-
-    payload = {
+    client = APIClient()
+    original_payload = {
         "title": "API Automation",
         "body": "Learning POST requests",
         "userId": 1
     }
-
     response = client.post(
         "https://jsonplaceholder.typicode.com/posts",
-        payload
+        original_payload
     )
 
     assert response.status_code == 201
@@ -35,3 +33,42 @@ def test_create_post():
     assert response_data["userId"] == 1
 
     print(response_data)
+
+def test_update_post_with_put():
+    client = APIClient()
+    payload = {
+        "id": 1,
+        "title": "Updated API Automation",
+        "body": "Updated using PUT",
+        "userId": 1
+    }
+    response=client.put(
+        "https://jsonplaceholder.typicode.com/posts/1",
+        payload
+    )
+    assert response.status_code== 200
+    response_data=response.json()
+    assert response_data["id"] == 1
+    assert response_data["title"] == "Updated API Automation"
+    assert response_data["body"] == "Updated using PUT"
+    print(response_data)
+
+def test_update_post_with_patch():
+    client = APIClient()
+    payload = {
+        "title": "Partially Updated Title"
+    }
+    response=client.patch("https://jsonplaceholder.typicode.com/posts/1",payload)
+    assert response.status_code== 200
+    response_data=response.json()
+    assert response_data["id"] == 1
+    assert response_data["title"] == "Partially Updated Title"
+
+    print(response_data)
+
+def test_delete_post():
+    client = APIClient()
+    response=client.delete("https://jsonplaceholder.typicode.com/posts/1")
+    assert response.status_code == 200
+
+    print(response.status_code)
