478b6c3c582c2a087384a3d18f4b81ed4179aba6
Prem123e <premkumarkanuganti123@gmail.com>
2026-09-13 12:50:19 +0530

    "Day 12: Add POST API automation"

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,10 +1,37 @@
 from api.api_client import APIClient
 
-def test_get_single():
-    client=APIClient()
+client = APIClient()
+def test_get_single_post():
+    response = client.get(
+        "https://jsonplaceholder.typicode.com/posts/1"
+    )
 
-    response=client.get("https://jsonplaceholder.typicode.com/posts/1")
     assert response.status_code == 200
-    response_data=response.json()
-    assert response_data["id"]== 1
+    response_data = response.json()
+
+    assert response_data["id"] == 1
+
+    print(response_data)
+
+def test_create_post():
+
+    payload = {
+        "title": "API Automation",
+        "body": "Learning POST requests",
+        "userId": 1
+    }
+
+    response = client.post(
+        "https://jsonplaceholder.typicode.com/posts",
+        payload
+    )
+
+    assert response.status_code == 201
+
+    response_data = response.json()
+
+    assert response_data["title"] == "API Automation"
+    assert response_data["body"] == "Learning POST requests"
+    assert response_data["userId"] == 1
+
     print(response_data)
