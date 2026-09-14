6cfe07faaacf8fc760ec717e65421c0cfcfbe853
Prem123e <premkumarkanuganti123@gmail.com>
2026-09-14 11:58:53 +0530

    "Day 17: Add reusable API client fixture"

---
+++ conftest.py
@@ -0,0 +1,9 @@
+import pytest
+
+from api.api_client import APIClient
+
+
+@pytest.fixture
+def api_client():
+
+    return APIClient()
--- tests/test_api.py
+++ tests/test_api.py
@@ -2,90 +2,104 @@ from utils.api_assertions import (
     assert_status_code,
     assert_json_field
 )
-from api.api_client import APIClient
-def test_get_single_post():
-    client = APIClient()
-    response = client.get(
+
+
+def test_get_single_post(api_client):
+
+    response = api_client.get(
         "https://jsonplaceholder.typicode.com/posts/1"
     )
 
     assert_status_code(response, 200)
     assert_json_field(response, "id", 1)
+
     print(response.json())
 
-def test_create_post():
-    client = APIClient()
-    original_payload = {
+
+def test_create_post(api_client):
+
+    payload = {
         "title": "API Automation",
         "body": "Learning POST requests",
         "userId": 1
     }
-    response = client.post(
+
+    response = api_client.post(
         "https://jsonplaceholder.typicode.com/posts",
-        original_payload
+        payload
     )
-    assert_status_code(response,201)
+
+    assert_status_code(response, 201)
+
     assert_json_field(
-    response,
-    "title",
-    "API Automation"
+        response,
+        "title",
+        "API Automation"
     )
 
     assert_json_field(
-    response,
-    "body",
-    "Learning POST requests"
+        response,
+        "body",
+        "Learning POST requests"
     )
 
     assert_json_field(
-    response,
-    "userId",
-    1
+        response,
+        "userId",
+        1
     )
+
     print(response.json())
 
-def test_update_post_with_put():
-    client = APIClient()
+
+def test_update_post_with_put(api_client):
+
     payload = {
         "id": 1,
         "title": "Updated API Automation",
         "body": "Updated using PUT",
         "userId": 1
     }
-    response=client.put(
+
+    response = api_client.put(
         "https://jsonplaceholder.typicode.com/posts/1",
         payload
     )
+
     assert_status_code(response, 200)
-    assert_json_field(
-        response,
-        "id",
-        1
-    )
+
+    assert_json_field(response, "id", 1)
+
     assert_json_field(
         response,
         "title",
         "Updated API Automation"
     )
+
     assert_json_field(
         response,
         "body",
         "Updated using PUT"
     )
+
     print(response.json())
 
-def test_update_post_with_patch():
-    client = APIClient()
+
+def test_update_post_with_patch(api_client):
+
     payload = {
         "title": "Partially Updated Title"
     }
-    response=client.patch("https://jsonplaceholder.typicode.com/posts/1",payload)
-    assert_status_code(response,200)
-    assert_json_field(
-        response,
-        "id",
-        1
+
+    response = api_client.patch(
+        "https://jsonplaceholder.typicode.com/posts/1",
+        payload
     )
+
+    assert_status_code(response, 200)
+
+    assert_json_field(response, "id", 1)
+
     assert_json_field(
         response,
         "title",
@@ -94,20 +108,26 @@ def test_update_post_with_patch():
 
     print(response.json())
 
-def test_delete_post():
-    client = APIClient()
-    response=client.delete("https://jsonplaceholder.typicode.com/posts/1")
-    assert_status_code(response,200)
+
+def test_delete_post(api_client):
+
+    response = api_client.delete(
+        "https://jsonplaceholder.typicode.com/posts/1"
+    )
+
+    assert_status_code(response, 200)
 
     print(response.status_code)
 
-def test_get_posts_by_user():
-    client=APIClient()
-    response=client.get(
+
+def test_get_posts_by_user(api_client):
+
+    response = api_client.get(
         "https://jsonplaceholder.typicode.com/posts",
-        params={"userId":1}
+        params={"userId": 1}
     )
-    assert_status_code(response,200)
+
+    assert_status_code(response, 200)
 
     response_data = response.json()
 
@@ -118,15 +138,20 @@ def test_get_posts_by_user():
 
     print(response_data)
 
-def test_get_post_with_headers():
-    client=APIClient()
-    headers={"Accept":"application/json"}
-    response=client.get("https://jsonplaceholder.typicode.com/posts/1",headers=headers)
-    assert_status_code(response,200)
-    assert_json_field(
-        response,
-        "id",
-        1
+
+def test_get_post_with_headers(api_client):
+
+    headers = {
+        "Accept": "application/json"
+    }
+
+    response = api_client.get(
+        "https://jsonplaceholder.typicode.com/posts/1",
+        headers=headers
     )
+
+    assert_status_code(response, 200)
+
+    assert_json_field(response, "id", 1)
 
     print(response.json())
